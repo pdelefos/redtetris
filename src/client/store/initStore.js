e210e8deff3c445fafafb5dfd2e56ff9d8f1bd09
pdelefos <pdelefos@gmail.com>
2018-02-27 16:02:11 +0100

    fixed erro compose missing

--- src/client/store/initStore.js
+++ src/client/store/initStore.js
@@ -1,15 +1,20 @@
-import { applyMiddleware, createStore } from "redux"
+import { applyMiddleware, createStore, compose } from "redux"
 
 import logger from "redux-logger"
 import thunk from "redux-thunk"
 
 import tetriStore from "../reducers"
 
-const middleware = applyMiddleware(thunk, logger)
-let store = createStore(
-  tetriStore,
-  middleware,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk, logger)
+  // other store enhancers if any
 )
+
+let store = createStore(tetriStore, enhancer)
 
 export default store
