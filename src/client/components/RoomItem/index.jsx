52efeda418c23457988bed56f4da4736c905e534
Philippe DELEFOSSE <pdelefos@e2r2p21.42.fr>
2018-03-20 14:22:48 +0100

    style on room-item

--- src/client/components/RoomItem/index.jsx
+++ src/client/components/RoomItem/index.jsx
@@ -1,20 +1,27 @@
 import React from "react"
+import "./style.scss"
+
 import { Link } from "react-router-dom"
 
 let RoomItem = ({ gameName, players, hashName, username, joinRoom }) => {
-	return (
-		<div className="room-item">
-			<span> Game Name: {gameName} </span>
-			<span> Number of players: {Object.keys(players).length} / 4</span>
-			<Link
-				className="button"
-				to={`/${hashName}[${username}]`}
-				onClick={() => joinRoom(hashName)}
-			>
-				Join
-      </Link>
-		</div>
-	)
+  return (
+    <div className="room-item">
+      <span className="room-item__game-name">{gameName}</span>
+      <span className="room-item__game-status">In Game</span>
+      <span className="room-item__player-count">
+        {Object.keys(players).length} / 4
+      </span>
+      <span className="room-item__action">
+        <Link
+          className="button"
+          to={`/${hashName}[${username}]`}
+          onClick={() => joinRoom(hashName)}
+        >
+          Join
+        </Link>
+      </span>
+    </div>
+  )
 }
 
 export default RoomItem
