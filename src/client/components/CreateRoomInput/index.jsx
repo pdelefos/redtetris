48ef59220dd37cd98a815dc0a312c792f3c7ad3d
Philippe DELEFOSSE <pdelefos@e3r2p23.42.fr>
2018-03-22 09:50:28 +0100

    now notifications desappear after 3s

--- src/client/components/CreateRoomInput/index.jsx
+++ src/client/components/CreateRoomInput/index.jsx
@@ -4,30 +4,30 @@ import { Redirect } from "react-router"
 let roomName = null
 
 const CreateRoomInput = ({ currentRoom, username, createRoom }) => {
-	if (!currentRoom)
-		return (
-			<div className="input-btn-grp">
-				<input
-					className="input--text"
-					type="text"
-					ref={input => (roomName = input)}
-				/>
-				<button
-					className="button"
-					onClick={() => {
-						createRoom(roomName.value)
-					}}
-				>
-					Create a room
+  if (!currentRoom)
+    return (
+      <div className="input-btn-grp">
+        <input
+          className="input--text"
+          type="text"
+          ref={input => (roomName = input)}
+        />
+        <button
+          className="button"
+          onClick={() => {
+            createRoom(roomName.value)
+          }}
+        >
+          Create a room
         </button>
-			</div>
-		)
-	else
-		return (
-			<div>
-				<Redirect to={`/${currentRoom}[${username}]`} />
-			</div>
-		)
+      </div>
+    )
+  else
+    return (
+      <div>
+        <Redirect to={`/${currentRoom}[${username}]`} />
+      </div>
+    )
 }
 
 export default CreateRoomInput
