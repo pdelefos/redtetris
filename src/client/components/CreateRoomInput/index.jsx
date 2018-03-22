cf30479a2103326a53879f8ed86959c12603f511
Philippe DELEFOSSE <pdelefos@e3r3p16.42.fr>
2018-03-22 13:21:08 +0100

    now we can create a room on enter press

--- src/client/components/CreateRoomInput/index.jsx
+++ src/client/components/CreateRoomInput/index.jsx
@@ -1,33 +1,39 @@
 import React from "react"
 import { Redirect } from "react-router"
 
-let roomName = null
+let roomNameInput = null
 
 const CreateRoomInput = ({ currentRoom, username, createRoom }) => {
-	if (!currentRoom)
-		return (
-			<div className="input-btn-grp">
-				<input
-					className="input--text"
-					type="text"
-					ref={input => (roomName = input)}
-				/>
-				<button
-					className="button"
-					onClick={() => {
-						createRoom(roomName.value)
-					}}
-				>
-					Create a room
+  if (!currentRoom)
+    return (
+      <div className="input-btn-grp">
+        <input
+          className="input--text"
+          type="text"
+          ref={input => (roomNameInput = input)}
+          onKeyDown={evt => handleKeyDown(createRoom, evt)}
+          tabIndex="0"
+        />
+        <button
+          className="button"
+          onClick={() => {
+            createRoom(roomNameInput.value)
+          }}
+        >
+          Create a room
         </button>
-			</div>
-		)
-	else
-		return (
-			<div>
-				<Redirect to={`/${currentRoom}[${username}]`} />
-			</div>
-		)
+      </div>
+    )
+  else
+    return (
+      <div>
+        <Redirect to={`/${currentRoom}[${username}]`} />
+      </div>
+    )
+}
+
+const handleKeyDown = (createRoomCallback, evt) => {
+  if (evt.key === "Enter") createRoomCallback(roomNameInput.value)
 }
 
 export default CreateRoomInput
--- src/server/server.js
+++ src/server/server.js
@@ -11,30 +11,30 @@ const config = yaml.load(path.join(__dirname, "config.yml"))
 const port = process.env.PORT || config.server.port
 
 const initApp = () => {
-	const app = express()
-	app.use(express.static(path.join(__dirname, "config.yml")))
-	app.use(bodyParser.urlencoded({ extended: false }))
-	app.use(bodyParser.json({ type: "*/*" }))
-	router(app)
-	return http.createServer(app)
+  const app = express()
+  app.use(express.static(path.join(__dirname, "config.yml")))
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.json({ type: "*/*" }))
+  router(app)
+  return http.createServer(app)
 }
 
 const initEngine = io => {
-	let manager = new Manager(io)
+  let manager = new Manager(io)
 }
 
 const wrapIOApp = server => {
-	const io = require("socket.io")(server)
-	return io
+  const io = require("socket.io")(server)
+  return io
 }
 
 export const initServer = () => {
-	const server = initApp()
-	const io = wrapIOApp(server)
-	server.listen(port, () => {
-		initEngine(io)
-		console.log("Server is running on localhost:" + port)
-	})
+  const server = initApp()
+  const io = wrapIOApp(server)
+  server.listen(port, () => {
+    initEngine(io)
+    console.log("Server is running on localhost:" + port)
+  })
 }
 
 initServer()
