5bdde061e694e9ec227f40df8313bb9def18d0c8
pdelefos <pdelefos@pdelefoss-MacBook-Pro.local>
2018-05-16 18:59:44 +0200

    1 player no end game fixed

--- webpack/webpack.config.production.js
+++ webpack/webpack.config.production.js
@@ -2,6 +2,7 @@ var path = require("path")
 var webpack = require("webpack")
 var merge = require("webpack-merge")
 var ExtractTextPlugin = require("extract-text-webpack-plugin")
+var FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 var sharedConf = require("./webpack.shared")
 
@@ -11,6 +12,7 @@ module.exports = merge(sharedConf, {
     new ExtractTextPlugin("[name].css"),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("production")
-    })
+    }),
+    new FaviconsWebpackPlugin('./src/client/favicon.png')
   ]
 })
--- webpack/webpack.shared.js
+++ webpack/webpack.shared.js
@@ -1,7 +1,6 @@
 var process = require("process")
 var path = require("path")
 var HtmlWebpackPlugin = require("html-webpack-plugin")
-var FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
   entry: ["react-hot-loader/patch", "./src/client/entry.js"],
@@ -58,7 +57,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       inject: true,
       template: "./src/client/index.html"
-    }),
-    new FaviconsWebpackPlugin('./src/client/favicon.png')
+    })
   ]
 }
