d1b3a5436cb096d24cd24befe1ebc20daf0d4719
pdelefos <pdelefos@pdelefoss-MacBook-Pro.local>
2018-05-16 16:48:29 +0200

    room name aligment fix

--- src/client/components/Header/index.jsx
+++ src/client/components/Header/index.jsx
@@ -11,12 +11,14 @@ let Header = ({ username, roomName }) => {
         <Logo />
       </span>
       <span className="header__middle">
-        <div className="room-name">
-          <span className="room-name__title">ROOM NAME</span>
-          <span className="room-name__value">
-            {roomName && roomName.toUpperCase()}
-          </span>
-        </div>
+        {roomName &&
+          <div className="room-name">
+            <span className="room-name__title">ROOM NAME</span>
+            <span className="room-name__value">
+              {roomName.toUpperCase()}
+            </span>
+          </div>
+        }
       </span>
       <span className="header__right">
         <img src={UserIcon} alt="user icon" className="icon--margin-right" />
