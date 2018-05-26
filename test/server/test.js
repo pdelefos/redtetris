a6e51544139cd5937b70d01a9e2b00bf13a82e7d
Philippe DELEFOSSE <pdelefos@e2r5p16.42.fr>
2018-05-26 14:59:34 +0200

    final little changes + auteur file

--- test/server/test.js
+++ test/server/test.js
@@ -3,7 +3,7 @@ import { initServer } from "../../src/server/server"
 
 describe("Fake server test", function() {
   it("Should instantiate the server", function(done) {
-    // initServer()
+    initServer()
     done()
   })
 
